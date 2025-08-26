64c368fc8bf66be809ed14193bde08080c692bf8
Mark Landeryou <samurai96@hotmail.com>
2025-08-26 01:42:07 -0400

    refactor: update counter setup and remove unused elements
    - Refactored the setupCounter function in counter.ts for improved readability.
    - Removed unused imports and elements from main.ts, replacing them with a welcome message.
    - Cleaned up style.css by removing unnecessary styles and applying Tailwind CSS classes.
    - Updated tsconfig.json for better module detection and linting options.
    - Added ESLint configuration for consistent code style and rules.
    - Created a new Vite configuration file to integrate Tailwind CSS.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,7 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+  <div class="mx-auto flex min-h-screen w-full flex-col items-center justify-center">
+    <p class="text-2xl  font-mono">Welcome to <span class="font-bold tracking-wide text-blue-600 font-serif text-3xl">Mark Landeryou dot ca</span>.<br>This site is a work in progress, please come back later.</p>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
